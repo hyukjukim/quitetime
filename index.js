ec354066865a4a475d2e351af6ca05014155ecc4
hyukjukim <hyukjukim@koreanair.com>
2017-02-03 18:35:02 +0900

    20170203

--- index.js
+++ index.js
@@ -89,6 +89,76 @@ app.use(bodyParser.urlencoded({
 //예를들어 http://example.com/category/id?_method=delete를 받으면 _method의 값인 delete을 읽어 해당 request의 HTTP method를 delete으로 바꿉니다.
 app.use(methodOverride("_method"));
 
+//사이트 root로 이동하는 경우 /kakaomsgs로 redirect 해준다.
+app.get("/", function(req, res) {
+    console.log('main come');
+});
+//root/kakaomsgs로 이동하는 경우. 내가 입력한 모든 Data를 찾아서 보여줍니다. kakaomsgs/index로 redirect 합니다.
+app.get("/kakaomsgs", function(req, res) {
+    Kakaomsg.find({}, function(err, kakaomsgs) {
+        if (err) return res.json(err);
+        res.render("kakaomsgs/index", {
+            kakaomsgs: kakaomsgs
+        });
+    });
+});
+// kakaomsgs/new"에 get 요청이 오는 경우 : 새로운 주소록을 만드는 form이 있는 views/kakaomsgs/new.ejs를 render합니다.
+app.get("/kakaomsgs/new", function(req, res) {
+    res.render("kakaomsgs/new");
+});
+// views/kakaomsgs/new.ejs 에서 post로 접근 create는 post로만 가능하다. submit 버튼 누르면 날아옴
+app.post("/kakaomsgs", function(req, res) {
+    Kakaomsg.create(req.body, function(err, kakaomsg) {
+        if (err) return res.json(err);
+        res.redirect("/kakaomsgs");
+    });
+});
+
+//req.params.id는 MONGO_DB에서 사용하는 ROWID 같은 개념이다.
+// views/kakaomsgs/index.ejs 에서 herf로 접근 이름을 클릭하면 MONGO_DB _id를 return으로 날려준다.
+app.get("/kakaomsgs/:id", function(req, res) {
+    Kakaomsg.findOne({
+        _id: req.params.id
+    }, function(err, kakaomsg) {
+        if (err) return res.json(err);
+        res.render("kakaomsgs/show", {
+            kakaomsg: kakaomsg
+        });
+        console.log('id' + req.params.id);
+    });
+});
+
+// views/kakaomsgs/show.ejs  에서 herf로 접근. 한 개의 Data만 표출 되므로 따로 선택은 필요 없음
+app.get("/kakaomsgs/:id/edit", function(req, res) {
+    Kakaomsg.findOne({
+        _id: req.params.id
+    }, function(err, kakaomsg) {
+        if (err) return res.json(err);
+        res.render("kakaomsgs/edit", {
+            kakaomsg: kakaomsg
+        });
+    });
+});
+
+// views/kakaomsgs/edit.ejs 에서
+app.put("/kakaomsgs/:id", function(req, res) {
+    Kakaomsg.findOneAndUpdate({
+        _id: req.params.id
+    }, req.body, function(err, kakaomsg) {
+        if (err) return res.json(err);
+        res.redirect("/kakaomsgs/" + req.params.id);
+    });
+});
+
+// kakaomsgs - destroy // 7
+app.delete("/kakaomsgs/:id", function(req, res) {
+    Kakaomsg.remove({
+        _id: req.params.id
+    }, function(err, kakaomsg) {
+        if (err) return res.json(err);
+        res.redirect("/kakaomsgs");
+    });
+});
 
 //KAKAO TALK
 app.get('/keyboard', function(req, res) {
