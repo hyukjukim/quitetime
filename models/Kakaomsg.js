6a826eda07daca485ed1d09933f7550d8379e4e9
hyukjukim <hyukjukim@koreanair.com>
2017-02-03 19:21:39 +0900

    20170203

---
+++ models/Kakaomsg.js
@@ -0,0 +1,24 @@
+// models/Kakaomsg.js
+
+
+var mongoose = require('mongoose');
+
+
+var kakaomsgSchema = mongoose.Schema({
+    user_key: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    type: {
+        type: String
+    },
+    content: {
+        type: String
+    }
+});
+//mongoose.model함수를 사용하여 kakaomsg schema의 model을 생성합니다 kakaomsg에 일반적으로 s가 붙어서 테이블 생성
+var Kakaomsg = mongoose.model("kakaomsg", kakaomsgSchema);
+
+module.exports = Kakaomsg;
---
+++ models/Kakaouser.js
@@ -0,0 +1,35 @@
+
+// models/Kakaouser.js
+
+
+var mongoose = require('mongoose');
+
+//user 관리를 위한 Schema를 생성합니다.
+var kakaouserSchema = mongoose.Schema({
+    user_key: {
+        type: String,
+        unique: true
+    },
+    name: {
+        type: String,
+    },
+    password: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    name_flag: {
+        type: String
+    },
+    password_flag: {
+        type: String
+    },
+    email_flag: {
+        type: String
+    }
+});
+//Kakaouser 변수로 테이블에 접근
+var Kakaouser = mongoose.model("kakaouser", kakaouserSchema);
+
+module.exports = Kakaouser;
